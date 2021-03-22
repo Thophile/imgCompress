64d94f242bd917d8d4b1dac2e583d8abcb895338
Thophile <montemonttheophile@gmail.com>
2021-03-22 14:34:41 +0100

    added image compression script

---
+++ compress.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+
+from PIL import Image
+import PIL
+
+def walk(path, r = False):
+    if os.path.isfile(path):
+        compress(path)
+    else :
+        for f in os.listdir(path):
+
+            if os.path.isdir(os.path.join(path,f)) & r:
+                print('Searching subfolder : ' + f)
+                walk(os.path.join(path,f)) 
+            else:
+                compress(os.path.join(path,f))
+def compress(f):
+    if f.endswith('.jpg') | f.endswith('.png') | f.endswith('.gif'):
+        Image.open(f).save(f,optimize=True,quality=85)
+        print(f'File : "{f}" compressed')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', action='store_true', default=False, help='Use recursion')
+parser.add_argument('--path', default=os.getcwd(), help='The path of the file or folder to compress default to current work directory')
+args = parser.parse_args()
+
+walk(args.path,args.r)
